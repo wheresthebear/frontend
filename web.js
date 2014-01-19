412c60a9426575b70732b30e83a80fe7ccadbbab
aimee rivers <aimee@aimeerivers.com>
2014-01-19 16:43:11 +0000

    Configure to use a public directory.

--- web.js
+++ web.js
@@ -4,6 +4,10 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.configure(function() {
+  app.use(express.static(__dirname + '/public'));
+});
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
