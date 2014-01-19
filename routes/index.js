f3d3a98b61df005779a4a9d4e623a2af63b6966f
Matthew Weaver <weavermj@Macintosh.local>
2014-01-19 18:08:04 +0000

    Tweaked nav bar.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 exports.home = function(req, res) {
   res.render('index', {
-      title: 'THIS IS THE INDEX PAGE',
-      description: 'Does it work',
+      title: 'wheresthebear.org',
+      description: 'Take me on a new adventure',
       author: '@weavermj'
   });
 };
 
 exports.woo = function(req, res) {
   res.render('woo', {
-      title: 'THIS IS THE WOO PAGE',
+      title: 'This is the woo page',
       description: 'Does it work',
       author: '@weavermj'
   });
