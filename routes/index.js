849a86a1c94de0b113c75216fd5a522b234d4893
Matthew Weaver <weavermj@Macintosh.local>
2014-01-19 17:30:22 +0000

    Added new routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 exports.home = function(req, res) {
-  res.render('woo', {
+  res.render('index', {
       title: 'THIS IS THE INDEX PAGE',
+      description: 'Does it work',
+      author: '@weavermj'
+  });
+};
+
+exports.woo = function(req, res) {
+  res.render('woo', {
+      title: 'THIS IS THE WOO PAGE',
       description: 'Does it work',
       author: '@weavermj'
   });
--- web.js
+++ web.js
@@ -12,8 +12,18 @@ app.configure(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
+
 app.get('/', routes.home);
 
+app.get('/woo', routes.woo);
+
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
+
+// app.get('/about', function(req, res) {
+//   res.send('About the bear');
+// });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
