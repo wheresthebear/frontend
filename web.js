18bdab06399698fee9317fcd5d6448a530e7d32a
Matthew Weaver <weavermj@Macintosh.local>
2014-01-19 17:02:29 +0000

    Added new layout and woo view.

--- web.js
+++ web.js
@@ -5,6 +5,8 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
   app.use(express.static(__dirname + '/public'));
 });
 
