f939aacd3eb326dcb06b8e58e03b74be55176c1e
aimee rivers <aimee@aimeerivers.com>
2014-01-19 16:06:47 +0000

    Demonstrate adding a second route.

--- web.js
+++ web.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/about', function(req, res) {
+  res.send('About the bear');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
