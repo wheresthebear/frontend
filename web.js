d757440f9f11dedbbb94d68830105c49df863a47
Matthew Weaver <weavermj@Macintosh.local>
2014-02-15 18:07:42 +0000

    Adds sendgrid support. Adds css fix for textarea bug with rows attribute.

--- web.js
+++ web.js
@@ -2,9 +2,15 @@ var express = require("express");
 var routes = require("./routes");
 var logfmt = require("logfmt");
 var app = express();
+var sendgrid  = require('sendgrid')(
+  'app21385780@heroku.com',
+  'harz9ury'
+);
 
 app.use(logfmt.requestLogger());
 app.use(express.urlencoded());
+
+
 
 app.configure(function() {
   app.set('views', __dirname + '/views');
@@ -19,7 +25,15 @@ app.get('/about', routes.about);
 app.get('/contact', routes.contact);
 
 app.post('/contact', function (req, res) {
-  console.log('this is:', req.body);
+  sendgrid.send({
+    to: 'weavermjw@googlemail.com',
+    from: 'sender@example.com',
+    subject: 'Hello World',
+    text: 'Sending email with NodeJS through SendGrid!'
+  }, function(err, json) {
+  if (err) { return console.error(err); }
+    console.log(json);
+  });
 });
 
 var port = process.env.PORT || 5000;
