78515e18c54a1e08fc1e6d636092a447d5a1556e
Matthew Weaver <weavermj@Macintosh.local>
2014-02-16 22:32:21 +0000

    Moves post logic out into router

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,21 @@
 exports.home = function(req, res) {
   res.render('index', {
       title: 'wheresthebear.org',
-      description: 'Take me on a new adventure',
-      author: '@weavermj'
+      description: 'Take me on a new adventure'
   });
 };
 
 exports.library = function(req, res) {
   res.render('library', {
       title: 'Style library',
-      description: '',
-      author: '@weavermj'
+      description: ''
   });
 };
 
 exports.about = function(req, res) {
   res.render('about', {
       title: 'About',
-      description: 'Find out what wheresthebear is here to do',
-      author: '@weavermj'
+      description: 'Find out what wheresthebear is here to do'
   });
 };
 
@@ -26,7 +23,38 @@ exports.contact = function(req, res) {
   res.render('contact', {
       title: 'Contact',
       description: 'Send me your story of how you found me',
-      author: '@weavermj'
+      errors: {}
   });
 };
 
+exports.contactPost = function(req, res) {
+ var errors;
+ req.assert('name', 'Name is required').notEmpty();  
+ req.assert('email', 'Email is required').notEmpty();
+ req.assert('email', 'Email must be a valid email address').isEmail();
+ errors = req.validationErrors();  
+ console.log(errors);
+  if( !errors){   
+    res.render('contact', { 
+      title: 'Contact',
+      message: 'Send me your story of how you found me',
+      errors: {}
+    });
+  }
+  else {   
+    res.render('contact', { 
+      title: 'Contact',
+      message: 'Send me your story of how you found me',
+      errors: errors
+    });
+  }
+  // sendgrid.send({
+  //   to: 'weavermjw@googlemail.com',
+  //   from: 'sender@example.com',
+  //   subject: 'Hello World',
+  //   text: 'Sending email with NodeJS through SendGrid!'
+  // }, function(err, json) {
+  // if (err) { return console.error(err); }
+  //   console.log(json);
+  // });
+};
--- web.js
+++ web.js
@@ -1,16 +1,16 @@
-var express = require("express");
-var routes = require("./routes");
-var logfmt = require("logfmt");
-var app = express();
-var sendgrid  = require('sendgrid')(
-  'app21385780@heroku.com',
-  'harz9ury'
-);
+var express = require("express"),
+    routes = require("./routes"),
+    logfmt = require("logfmt"),
+    app = express(),
+    sendgrid  = require('sendgrid')(
+      'app21385780@heroku.com',
+      'harz9ury'
+    ),
+    expressValidator = require('express-validator');
 
 app.use(logfmt.requestLogger());
 app.use(express.urlencoded());
-
-
+app.use(expressValidator()); 
 
 app.configure(function() {
   app.set('views', __dirname + '/views');
@@ -24,17 +24,7 @@ app.get('/library', routes.library);
 app.get('/about', routes.about);
 app.get('/contact', routes.contact);
 
-app.post('/contact', function (req, res) {
-  sendgrid.send({
-    to: 'weavermjw@googlemail.com',
-    from: 'sender@example.com',
-    subject: 'Hello World',
-    text: 'Sending email with NodeJS through SendGrid!'
-  }, function(err, json) {
-  if (err) { return console.error(err); }
-    console.log(json);
-  });
-});
+app.post('/contact', routes.contactPost);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
