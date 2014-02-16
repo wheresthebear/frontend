bac8254a50863856daa33accf824d8bb598408b7
Matthew Weaver <weavermj@Macintosh.local>
2014-02-16 23:12:04 +0000

    Adds basic error handling to contact form

--- routes/index.js
+++ routes/index.js
@@ -23,17 +23,21 @@ exports.contact = function(req, res) {
   res.render('contact', {
       title: 'Contact',
       description: 'Send me your story of how you found me',
-      errors: {}
+      errors: {},
+      name: '',
+      email: '',
+      story: ''
   });
 };
 
 exports.contactPost = function(req, res) {
- var errors;
- req.assert('name', 'Name is required').notEmpty();  
- req.assert('email', 'Email is required').notEmpty();
- req.assert('email', 'Email must be a valid email address').isEmail();
- errors = req.validationErrors();  
- console.log(errors);
+  var errors, values;
+  req.assert('name', 'Name is required').notEmpty();  
+  req.assert('email', 'Email is required').notEmpty();
+  req.assert('email', 'Email must be a valid email address').isEmail();
+  req.assert('story', 'Story must be at least 25 characters').len(25);
+  errors = req.validationErrors();  
+
   if( !errors){   
     res.render('contact', { 
       title: 'Contact',
@@ -45,7 +49,10 @@ exports.contactPost = function(req, res) {
     res.render('contact', { 
       title: 'Contact',
       message: 'Send me your story of how you found me',
-      errors: errors
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      story: req.body.story
     });
   }
   // sendgrid.send({
